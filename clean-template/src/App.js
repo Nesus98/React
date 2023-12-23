4e4ec3b947329fda20e15287b99de9a1850ef068
Nesus98 <jesusarroyo30.98@gmail.com>
2023-12-23 13:26:12 +0100

    cleanTemplateReady

---
+++ clean-template/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        recusandae necessitatibus sunt culpa ab, tempora sapiente quia nihil.
+        Quia voluptas mollitia dolor deserunt reprehenderit quas vel, ex rerum
+        doloremque nisi unde ratione quidem facilis corporis nihil laudantium!
+        Voluptates reprehenderit id quis architecto asperiores quod iure sequi
+        odit optio, esse delectus?
+      </p>
+    </div>
+  );
+}
+
+export default App;
+
