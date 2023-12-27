536188f5c7319ae1fb710efaa544e81633d2255d
Nesus98 <jesusarroyo30.98@gmail.com>
2023-12-27 23:40:45 +0100

    ejemplo minimo componentes

--- components/src/App.js
+++ components/src/App.js
@@ -1,17 +1,13 @@
-import "./App.css";
+import Title from "./components/Title";
+import Paragraph from "./components/Paragraph";
+import "./App.css"
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-        recusandae necessitatibus sunt culpa ab, tempora sapiente quia nihil.
-        Quia voluptas mollitia dolor deserunt reprehenderit quas vel, ex rerum
-        doloremque nisi unde ratione quidem facilis corporis nihil laudantium!
-        Voluptates reprehenderit id quis architecto asperiores quod iure sequi
-        odit optio, esse delectus?
-      </p>
+    <div className="main">
+      <Title></Title>
+      <Paragraph></Paragraph>
+      <Paragraph></Paragraph>
     </div>
   );
 }
---
+++ components/src/components/Paragraph.js
@@ -0,0 +1,11 @@
+import "./Paragraph.css";
+
+function Paragraph() {
+  return ( <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      recusandae necessitatibus sunt culpa ab, tempora sa
+    </p>
+  );
+}
+
+export default Paragraph;
