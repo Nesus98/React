62f4b369ad32b2dfd4131dbcce6028996561defa
Nesus98 <jesusarroyo30.98@gmail.com>
2023-12-28 09:29:03 +0100

    adding new components

--- components/src/App.js
+++ components/src/App.js
@@ -1,13 +1,13 @@
-import Title from "./components/Title";
-import Paragraph from "./components/Paragraph";
 import "./App.css"
+import Welcome from "./components/Welcome";
 
 function App() {
+  const today =new Date(Date.now()).toLocaleDateString(); 
   return (
     <div className="main">
-      <Title></Title>
-      <Paragraph></Paragraph>
-      <Paragraph></Paragraph>
+      <Welcome name="Laura" today={today}></Welcome>
+      <Welcome name="Jaen" today={today}></Welcome>
+      <Welcome name="World" today={today}></Welcome>
     </div>
   );
 }
