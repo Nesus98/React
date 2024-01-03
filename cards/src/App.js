e93030d17faca2f62bf210cc8644308f790c41c3
Nesus98 <jesusarroyo30.98@gmail.com>
2024-01-04 00:38:00 +0100

    finished

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,79 +1,25 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faStar,
-  faEnvelope,
-  faDollarSign,
-  faAddressBook,
-  faPhoneFlip,
-  faCircleCheck,
-} from "@fortawesome/free-solid-svg-icons";
 // import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import "./App.css";
 import CardWrapper from "./components/CardWrapper";
-import freelancer1 from "./assets/img/freelancer1.jpg";
+import CardImg from "./components/CardImg";
+import CardInfo from "./components/CardInfo";
+import { freelancers } from "./db/freelancers";
+import CardsContainer from "./components/CardsContainer";
 
 function App() {
+  console.log(freelancers);
 
-  const freelancer = [ 
-    {
-      id : 0,
-      title : "MR",
-      lastName : "Rick",
-      role : "UI/UX Designer",
-      fee : 85,
-      reviews : 105
-  },
-]
-
-  return ( <div className="card-container">
-    <CardWrapper>
-      <div className="img-container">
-        <div className="img-wrapper">
-          <img src={freelancer1} alt="Freelancer-one" />
-          <span className="dot"></span>
-        </div>
-      </div>
-      <div className="info-container">
-        <h3>MR RICK</h3>
-        <p>UI/UX Designer</p>
-        <div className="hire">
-          <div>
-            Rate: <span className="price">$85/hr</span>{" "}
-          </div>
-          <button className="hire-button">Hire Me</button>
-        </div>
-        <div className="rating">
-          <div>5.0</div>
-          <div>
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-          </div>
-          <div>(105 Reviews)</div>
-        </div>
-        <div className="icons">
-          <div className="verified">
-            <FontAwesomeIcon icon={faCircleCheck} />
-            <span>Verified</span>
-          </div>
-          <span className="circle available">
-            <FontAwesomeIcon icon={faDollarSign} />
-          </span>
-          <span className="circle available">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </span>
-          <span className="circle not-available">
-            <FontAwesomeIcon icon={faAddressBook} />
-          </span>
-          <span className="circle not-available">
-            <FontAwesomeIcon icon={faPhoneFlip} />
-          </span>
-        </div>
-      </div>
-    </CardWrapper>
-  </div>
+  return (
+    <CardsContainer>
+      {freelancers.map((freelancer, index) => {
+        return (
+          <CardWrapper key={index} >
+            <CardImg freelancer={freelancer}></CardImg>
+            <CardInfo freelancer={freelancer}></CardInfo>
+          </CardWrapper>
+        );
+      })}
+    </CardsContainer>
   );
 }
 
---
+++ cards/src/db/freelancers.js
@@ -0,0 +1,38 @@
+import fl1 from "../assets/img/freelancer1.jpg";
+import fl2 from "../assets/img/freelancer2.jpg";
+import fl3 from "../assets/img/freelancer3.jpg";
+
+export const freelancers = [
+    {
+      "id": 0,
+      "img":{"path":fl1,"text":"Freelancer 1"},
+      "title": "MR.",
+      "lastName": "RICK",
+      "role": "UI/UX Designer",
+      "fee": 85,
+      "reviews": 105
+    },
+    {
+        "id": 1,
+        "img":{"path":fl2,"text":"Freelancer 2"},
+        "title": "MORTY ",
+        "lastName": "JR",
+        "role": "Web developer",
+      "fee": 80,
+      "reviews": 110
+    },
+    {
+        "id": 2,
+        "img":{"path":fl3,"text":"Freelancer 3"},
+      "title": "SID",
+      "lastName": "ICEAGE",
+      "role": "Web Designer",
+      "fee": 79,
+      "reviews": 100
+    }
+  ];
+  
+
+  
+  
+  
